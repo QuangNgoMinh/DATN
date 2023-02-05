12a76f6b79d6b46850b0e1aa62c70c34ef6c8fbe
Ngô Minh Quang <76208637+quangkeo125@users.noreply.github.com>
2023-02-05 16:28:43 +0700

    create project

---
+++ app/Http/Livewire/Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Dashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard')->layout('layout.app');
+    }
+
+    /**
+     */
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Livewire\Author;
+use App\Http\Livewire\Book;
+use App\Http\Livewire\Dashboard;
+use App\Http\Livewire\Category;
+use App\Http\Livewire\IssueBook;
+use App\Http\Livewire\Loign;
+use App\Http\Livewire\Publisher;
+use App\Http\Livewire\Report;
+use App\Http\Livewire\Setting;
+use App\Http\Livewire\Student;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', Dashboard::class);
+
+
