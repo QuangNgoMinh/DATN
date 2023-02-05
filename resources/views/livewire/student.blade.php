dca61cdb69988830caaa211f0b4ec7821886c746
Ngô Minh Quang <76208637+quangkeo125@users.noreply.github.com>
2023-02-05 22:42:19 +0700

    Without login, logout 2

--- resources/views/livewire/student.blade.php
+++ resources/views/livewire/student.blade.php
@@ -143,7 +143,7 @@
                 class="w-full py-3 px-2 my-4 text-sm text-gray-700 placeholder-gray-900 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-dark focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input">
                 <option selected>Enter the Gender</option>
                 <option value="1">Male</option>
-                <option value="0">FeMale</option>
+                <option value="0">Female</option>
 
             </select>
             @error('gender')
@@ -197,7 +197,7 @@
                 class="w-full py-3 px-2 my-4 text-sm text-gray-700 placeholder-gray-900 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-dark focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input">
                 <option selected>Enter the Gender</option>
                 <option value="1">Male</option>
-                <option value="0">FeMale</option>
+                <option value="0">Female</option>
 
             </select>
 
